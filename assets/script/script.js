68897f8619db0b838ace64a67a61bbfc48390ef9
Shea Mullaney <seagda@gmail.com>
2021-01-06 09:48:24 -0800

    setup initial structure

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+// TODO: Create a start button with click event attached.
+    // TODO: Create a timer div and update that
+    // TODO: refresh the question-body div with first of series of multiple-choice questions
+            //TODO: if correct answer, move to next question and increment and save score and time-left (or time taken? or product of score with time-left) variable.
+            //TODO: else subtract from timer, clear and display next question
+// TODO: When timer equals 0, end game
+// TODO: When all questions answered, end game
+// TODO: all user to save initials
+
+
+function startQuiz() {
+
+}
+
+function nextQ() {
+
+}
+
+function calcScore(){
+
+}
